630c26872de7580a83e5438d8d14b21612cdfa4b
Shawn Tolidano <shawn@tolidano.com>
2020-12-05 23:51:24 -0500

    Working

--- cli/ws_client.py
+++ cli/ws_client.py
@@ -7,25 +7,19 @@ import websockets
 from utils import encode, decode
 
 if len(sys.argv) < 2:
-    print(f"Sytnax {sys.argv[0]} room (delay)")
+    print(f"Syntax {sys.argv[0]} [room]")
     sys.exit(-1)
 
-room = sys.argv[1]
-slow = 0.0
-if len(sys.argv) > 2:
-    slow = float(sys.argv[2])
-
-trigger_count = 5000.0
-if slow > 0:
-    trigger_count /= (1 + slow) * 100
+room: str = sys.argv[1]
+trigger_count: float = 5000.0
 
 
 async def reader(websocket):
-    count = 0
-    seq = 0
-    last_time = time.monotonic()
-    client_id = None
-    last_msg_id = None
+    count: int = 0
+    seq: int = 0
+    last_time: int = time.monotonic()
+    client_id: int = None
+    last_msg_id: int = None
 
     async for message_raw in websocket:
         count += 1
@@ -37,15 +31,13 @@ async def reader(websocket):
             msg_id = msg["msg_id"]
             if last_msg_id is None:
                 last_msg_id == msg_id
-            else:
-                if msg_id != (last_msg_id + 1):
+            elif msg_id != (last_msg_id + 1):
                     print(last_msg_id, msg_id)
                     raise Exception("bad msg sequence")
 
-        if msg["type"] == "ping" and client_id == msg["client_id"]:
-            if msg["seq"] != seq:
-                print(seq, message_raw)
-                raise Exception("bad message seq")
+        if msg["type"] == "ping" and client_id == msg["client_id"] and msg["seq"] != seq:
+            print(seq, message_raw)
+            raise Exception("bad message seq")
 
         if count >= trigger_count:
             next_time = time.monotonic()
@@ -57,20 +49,20 @@ async def reader(websocket):
             seq += 1
             await websocket.send(encode({"type": "ping", "seq": seq}))
 
-        if slow > 0:
-            await asyncio.sleep(slow)
-
 
 async def connect():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        print("Connecting")
-        await websocket.send(encode({"type": "join", "room": room}))
-        consumer_task = asyncio.ensure_future(reader(websocket))
-        done = await asyncio.wait(
-            [consumer_task],
-            return_when=asyncio.FIRST_COMPLETED,
-        )
+    uri: str = "ws://localhost:8765"
+    try:
+        async with websockets.connect(uri) as websocket:
+            print("Connecting")
+            await websocket.send(encode({"type": "join", "room": room}))
+            consumer_task = asyncio.ensure_future(reader(websocket))
+            done = await asyncio.wait(
+                [consumer_task],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+    except IOError as error:
+        print("Could not connect - is the server running? Do you have network access?")
 
 
 asyncio.get_event_loop().run_until_complete(connect())
